a0ef79945a4b3a92f9520462a51d431cc5bcc4d5
Zanarddi <gustavo.zanardin@gmail.com>
2023-05-22 11:47:08 -0300

    Mudando o arquivo esp32, para verificar o estado da vaga após 10 segundos, falta testar com o equipamento fisico

--- IOT/esp32.c
+++ IOT/esp32.c
@@ -2,6 +2,10 @@
 #include <HTTPClient.h>
 #include <NewPing.h>
 
+#define STATE_INVALID -1
+#define STATE_EMPTY 0
+#define STATE_OCCUPIED 1
+#define DISTANCE_MAX 100
 #define TRIG_PIN_1 12
 #define ECHO_PIN_1 14
 // #define TRIG_PIN_2 13
@@ -13,38 +17,38 @@ NewPing sonar1(TRIG_PIN_1, ECHO_PIN_1);
 const char *deviceId = "*****";
 const char *ssid_Router = "****";
 const char *password_Router = "****";
-const char* apiEndpoint = "****";
+const char *apiEndpoint = "****";
 
 HTTPClient httpClient;
 WiFiClient wifiClient;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   delay(2000);
 
   Serial.println("Setup start");
 
-
   WiFi.begin(ssid_Router, password_Router);
   Serial.println(String("Connecting to ") + ssid_Router);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print("Connecting to Wifi...");
   }
   Serial.println("\nConnected, IP address: ");
   Serial.println(WiFi.localIP());
 
-
   Serial.println("Setup End");
 }
 
-
-void sendInfo(int dist1, int dist2){
+void sendInfo(int state1, int state2)
+{
   httpClient.begin(wifiClient, apiEndpoint);
 
-  //httpClient.addHeader("Content-Type", "application/json");
+  // httpClient.addHeader("Content-Type", "application/json");
 
-  String requestBody = "{\"device_id\":\"" + String(deviceId) + "\",\"distances\":[" + dist1 + "," + dist2 + "]}";
+  String requestBody = "{\"device_id\":\"" + String(deviceId) + "\",\"states\":[" + state1 + "," + state2 + "]}";
   Serial.println(requestBody);
   int httpResponseCode = httpClient.POST(requestBody);
   String response = httpClient.getString();
@@ -58,21 +62,66 @@ void sendInfo(int dist1, int dist2){
   httpClient.end();
 }
 
+int validateSensor(int state, int distance){
+  if(distance == 0){
+    return STATE_INVALID;
+  }
+  else if(distance <= DISTANCE_MAX){
+    return STATE_OCCUPIED;
+  }
+  else if(distance > DISTANCE_MAX){
+    return STATE_EMPTY;
+  }
+  else{
+    return STATE_INVALID;
+  }
+}
 
-void loop() {
+void loop()
+{
 
   unsigned int distance1 = 0;
   unsigned int distance2 = 0;
 
+  int state1 = STATE_INVALID;
+  int state2 = STATE_INVALID;
+
   distance1 = sonar1.ping_cm();
   // distance2 = sonar2.ping_cm();
+
+  state1 = validateSensor(state1, distance1);
+  // state2 = validateSensor(state2, distance2);
 
   // Serial.println(String("{ device: ") + deviceId + String("}"));
   // Serial.println(String("{ sensor_1: ") + distance1 + String("}"));
   // Serial.println(String("{ sensor_2: ") + distance2 + String("}"));
   // Serial.println(String("-----"));
 
-  sendInfo(distance1, distance2);
+  delay(10000);
 
-  delay(5000);
+  distance1 = sonar1.ping_cm();
+  // distance2 = sonar2.ping_cm();
+
+  if (distance1 <= DISTANCE_MAX && distance1 > 0){
+    if(state1 == STATE_OCCUPIED){
+      state1 = STATE_OCCUPIED;
+    }
+  }
+  else{
+    state1 = validateSensor(state1, distance1);
+  }
+
+  // if (distance2 <= DISTANCE_MAX && distance2 > 0){
+  //   if(state2 == STATE_OCCUPIED){
+  //     state2 = STATE_OCCUPIED;
+  //   }
+  // }
+  // else{
+  //   state2 = validateSensor(state2, distance2);
+  // }
+
+
+  sendInfo(state1, state2);
+
+  delay(60000);
 }
